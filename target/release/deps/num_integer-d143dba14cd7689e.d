5cc0d2d922447318bcbeb922773f724bcbccf44c
Quin Darcy <quintondarcy@csus.edu>
2022-10-02 21:36:14 -0700

    Delete target/release/deps directory

--- target/release/deps/num_integer-d143dba14cd7689e.d
+++
@@ -1,9 +0,0 @@
-/home/arbegla/Projects/Rust/binaries/shasher256/target/release/deps/num_integer-d143dba14cd7689e.rmeta: /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs
-
-/home/arbegla/Projects/Rust/binaries/shasher256/target/release/deps/libnum_integer-d143dba14cd7689e.rlib: /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs
-
-/home/arbegla/Projects/Rust/binaries/shasher256/target/release/deps/num_integer-d143dba14cd7689e.d: /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs /home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs
-
-/home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs:
-/home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs:
-/home/arbegla/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs:
--- target/release/deps/shasher256-ad4f809233d81f72.d
+++
@@ -1,5 +0,0 @@
-/home/arbegla/Projects/Rust/binaries/shasher256/target/release/deps/shasher256-ad4f809233d81f72: src/main.rs
-
-/home/arbegla/Projects/Rust/binaries/shasher256/target/release/deps/shasher256-ad4f809233d81f72.d: src/main.rs
-
-src/main.rs:
